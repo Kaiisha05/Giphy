775ba3920e56a7a44d218db2cf50f63ad60e4364
Kaiisha05 <Kaiisha@yahoo.com>
2019-10-11 12:05:46 -0400

    curly bracket missing from 1st function. CHnaged variable name within queryURL to data attribute name so it wouldn't continue to call the iniital button images.

--- assets/javascript/giphy2.js
+++ assets/javascript/giphy2.js
@@ -15,77 +15,86 @@ function displayActivityImages() {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
+        console.log(response);
 
-        var activityDiv = $("<div>");
-        activityDiv.addClass("activity");
-        var rating = response.data.rating;
-        console.log(rating);
+        var results = response.data;
 
-        var para = $("<p>").text("Rating: " + rating);
+        for (var i = 0; i < results.length; i++) {
 
-        // display the rating
-        activityDiv.append(para);
+            var rating = results[i].rating;
 
-        // retrieving the URL for the image
-        var imageURL = response.data.images.fixed_height_still.url;
+            console.log(rating);
+            // var images = results[i].images.fixed_height_small;
 
-        // create an element to hold the image
 
-        var image = $("<img>").attr("src", imageURL);
+            var activityDiv = $("<div>");
+            activityDiv.addClass("activity");
+            // var rating = response.data.rating;
+            // console.log(rating);
 
-        // append the image
-        activityDiv.append(image);
+            var para = $("<p>").text("Rating: " + rating);
 
-        // putting the activity and it's rating above the previous activiy
-        $("#images-holder").prepend(activityDiv);
+            //         // display the rating
+            activityDiv.append(para);
+
+            //         // retrieving the URL for the image
+            var imageURL = results[i].images.fixed_height_still.url;
+
+            //         // create an element to hold the image
+
+            var image = $("<img>").attr("src", imageURL);
+
+            //         // append the image
+            activityDiv.append(image);
+
+            //         // putting the activity and it's rating above the previous activiy
+            $("#images-holder").prepend(activityDiv);
+
+        }
+
+    });
+}
+    // // function for displaying activity info
+    function renderButtons() {
+
+        $("#buttons-holder").empty();
+        //     // loop through the array of movies
+        for (let v = 0; v < activities.length; v++) {
+
+            //         // dynamically generate the buttons for each activity in the array
+
+            var a = $("<button>");
+            //         // adding a class of activity-btn to my button
+            a.addClass("activity-btn");
+            //         // adding a data-attribute 
+            a.attr("data-name", activities[v]);
+            //         // providing the initial button text
+            a.text(activities[v]);
+            //         // adding the button to the buttons div
+            $("#buttons-holder").append(a);
+
+
+        }
+    }
+
+
+    // // this function handles events when an activity button is clicked
+    $("#add-activity").on("click", function (event) {
+        event.preventDefault();
+        //     // this line grabs the input from the textbox
+        var activity = $("#user-input").val().trim();
+
+        //     // adding movie from the textbox to our array
+        activities.push(activity);
+        // $("#buttons-holder").empty();
+        //     // calling renderButtons which handles the processing of the actiivty array
+        renderButtons();
+
     });
 
-}
+    // // adding a clicked event listener to all elements with a class of "button"
+    $(document).on("click", ".activity-btn", displayActivityImages);
 
-
-// function for displaying activity info
-function renderButtons() {
-
-$("#buttons-holder").empty();
-    // loop through the array of movies
-    for (let v = 0; v < activities.length; v++) {
-
-        // dynamically generate the buttons for each activity in the array
-
-        var a = $("<button>");
-        // adding a class of activity-btn to my button
-        a.addClass("activity-btn");
-        // adding a data-attribute 
-        a.attr("data-name", activities[v]);
-        // providing the initial button text
-        a.text(activities[v]);
-        // adding the button to the buttons div
-        $("#buttons-holder").append(a);
-
-        
-    }
-}
-
-
-// this function handles events when an activity button is clicked
-$("#add-activity").on("click", function (event) {
-    event.preventDefault();
-    // this line grabs the input from the textbox
-    var activity = $("#user-input").val().trim();
-
-    // adding movie from the textbox to our array
-    activities.push(activity);
-    // $("#buttons-holder").empty();
-    // calling renderButtons which handles the processing of the actiivty array
+    // // calling the renderButtons function to display the initial buttons
     renderButtons();
-   
-});
-
-// adding a clicked event listener to all elements with a class of "button"
-$(document).on("click", "activity-btn", displayActivityImages);
-
-// calling the renderButtons function to display the initial buttons
-renderButtons();
-
-
 
